4b75fe90b60fea62f779a4ac4e0508ae9597b1da
tong <tong@disktree.net>
2022-11-21 19:09:40 +0100

    Fix projects

--- ui_events/Sources/arm/MyTrait.hx
+++ ui_events/Sources/arm/MyTrait.hx
@@ -3,10 +3,11 @@ package arm;
 class MyTrait extends iron.Trait {
 	public function new() {
 		super();
-		armory.system.Event.add("button", onEvent);
+		armory.system.Event.add("move_box", onEvent);
 	}
 
 	function onEvent() {
+        trace("move_box");
 		var loc = object.transform.loc;
 		loc.y += 1;
 		if (loc.y > 4) loc.y -= 8;
